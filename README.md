241fe71a13385dfecc0e87762e14dc02aaccdd81
Ben B <bbarantschik@gmail.com>
2017-04-08 18:55:09 -0700

    added simple ajax request, local app through electronify-server

---
+++ README.md
@@ -0,0 +1,4 @@
+Installation:
+```
+npm3 install watchdog
+```
---
+++ localapp/main.js
@@ -0,0 +1,6 @@
+var electronify = require('electronify-server');
+
+electronify({
+  url: 'http://127.0.0.1:8000',
+  noServer: true
+});
