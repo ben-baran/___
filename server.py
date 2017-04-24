92efd9ebbda94e3aac3366aaa17b576858b704b0
ben-baran <bbarantschik@gmail.com>
2017-04-23 22:45:47 -0700

    Divided into message types

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import json
 import queue
 import time
 
+import os
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -14,7 +16,7 @@ message_queue = queue.Queue()
 
 class EventSocketPusher(FileSystemEventHandler):
     def on_any_event(self, event):
-        message_queue.put(event.event_type + ' ' + event.src_path)
+        message_queue.put([event.event_type, event.src_path])
 
 
 settings = json.load(open('gibit.json'))
@@ -26,10 +28,16 @@ for path in settings['watch']:
     observer.start()
 
 
-async def hello(websocket, path):
+async def filesys_info(websocket, path):
+    for settings_root_path in settings['watch']:
+        await websocket.send(json.dumps({'type': 'root_directory', 'path': settings_root_path}))
+    # await websocket.send(json.dumps([entry for entry in os.walk('./test')]))
+
     while True:
         while not message_queue.empty():
-            message = message_queue.get()
+            next_change = message_queue.get()
+            message_data = {'type': 'system_change', 'change_type': next_change[0], 'change_dir': next_change[1]}
+            message = json.dumps(message_data)
             await websocket.send(message)
             print(message)
 
@@ -37,13 +45,13 @@ async def hello(websocket, path):
         # print("< {}".format(name))
         name = "auto"
 
-        greeting = "Hello {}!".format(name)
-        await websocket.send(greeting)
-        print("> {}".format(greeting))
+        # greeting = "Hello {}!".format(name)
+        # await websocket.send(greeting)
+        # print("> {}".format(greeting))
         time.sleep(1)
 
 
-start_server = websockets.serve(hello, 'localhost', 8787)
+start_server = websockets.serve(filesys_info, 'localhost', 8787)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
